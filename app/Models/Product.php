65007cd875fb4cd72b1c36e1d1e0bda8c8704cb1
tansibMuttakin <tansib.muhaimin@northsouth.edu>
2020-11-17 16:19:53 +0600

    relationship created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Review;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
+
