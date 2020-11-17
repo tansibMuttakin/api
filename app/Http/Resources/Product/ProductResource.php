8a90d8aab9800b7a3c7d5bf5023dbbdb70d38123
tansibMuttakin <tansib.muhaimin@northsouth.edu>
2020-11-17 18:01:17 +0600

    modified product details in ProductResource.php

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,15 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock == 0 ? "out of stock" :$this->stock,
             'discount'=>$this->discount,
+            'totalPrice'=>round((1-$this->discount/100) * $this->price,2),
+            'rating'=>$this->reviews->count() > 0 ? 
+                round($this->reviews->sum('star')/$this->reviews->count(),2)
+                :"no rating yet",
+            'href'=>[
+                'reviews'=>route('reviews.index',$this->id),
+            ],
         ];
     }
 }
