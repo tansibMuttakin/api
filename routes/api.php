a4a77d88a285375f7b462930aa7d76768ea48ed8
tansibMuttakin <tansib.muhaimin@northsouth.edu>
2020-11-17 15:32:57 +0600

    created model -a

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +19,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products', ProductController::class);
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('/{product}/reviews',ReviewController::class);
 });
