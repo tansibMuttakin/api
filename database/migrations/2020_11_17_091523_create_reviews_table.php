0447da58c2f9984dfdc367c929688602a32c5216
tansibMuttakin <tansib.muhaimin@northsouth.edu>
2020-11-17 15:47:22 +0600

    migration created for products and reviews

--- database/migrations/2020_11_17_091523_create_reviews_table.php
+++ database/migrations/2020_11_17_091523_create_reviews_table.php
@@ -15,6 +15,11 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer');
+            $table->string('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
